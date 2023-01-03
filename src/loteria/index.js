d7a40d5392685eeff7c318e1646d9e70ad264ce6
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-01-03 18:07:20 -0300

    Results data Loteria

--- src/loteria/index.js
+++ src/loteria/index.js
@@ -22,11 +22,11 @@ module.exports = async () => {
     numbersStart: 2,
     numbersFinal: 16,
     winners: [
-      { name: 'Ganhadores 15 números', total: 18, amount: 24 },
-      { name: 'Ganhadores 14 números', total: 21, amount: 25 },
-      { name: 'Ganhadores 13 números', total: 21, amount: 26 },
-      { name: 'Ganhadores 12 números', total: 22, amount: 27 },
-      { name: 'Ganhadores 11 números', total: 23, amount: 28 },
+      { name: '15 números', total: 18, amount: 24 },
+      { name: '14 números', total: 21, amount: 25 },
+      { name: '13 números', total: 21, amount: 26 },
+      { name: '12 números', total: 22, amount: 27 },
+      { name: '11 números', total: 23, amount: 28 },
     ],
   });
   
@@ -37,9 +37,9 @@ module.exports = async () => {
     numbersStart: 2,
     numbersFinal: 6,
     winners: [
-      { name: 'Ganhadores Quina', total: 8, amount: 10 },
-      { name: 'Ganhadores Quadra', total: 11, amount: 12 },
-      { name: 'Ganhadores Terno', total: 13, amount: 14 },
+      { name: 'Quina', total: 8, amount: 10 },
+      { name: 'Quadra', total: 11, amount: 12 },
+      { name: 'Terno', total: 13, amount: 14 },
     ],
   });
   
@@ -50,12 +50,12 @@ module.exports = async () => {
     numbersStart: 2,
     numbersFinal: 21,
     winners: [
-      { name: 'Ganhadores 20 números', total: 23, amount: 31 },
-      { name: 'Ganhadores 19 números', total: 26, amount: 32 },
-      { name: 'Ganhadores 18 números', total: 27, amount: 33 },
-      { name: 'Ganhadores 17 números', total: 28, amount: 34 },
-      { name: 'Ganhadores 16 números', total: 29, amount: 35 },
-      { name: 'Ganhadores nenhum número', total: 30, amount: 36 },
+      { name: '20 números', total: 23, amount: 31 },
+      { name: '19 números', total: 26, amount: 32 },
+      { name: '18 números', total: 27, amount: 33 },
+      { name: '17 números', total: 28, amount: 34 },
+      { name: '16 números', total: 29, amount: 35 },
+      { name: 'nenhum número', total: 30, amount: 36 },
     ],
   });
   
@@ -66,10 +66,10 @@ module.exports = async () => {
     numbersStart: 2,
     numbersFinal: 7,
     winners: [
-      { name: 'Ganhadores Sena', total: 9, amount: 11 },
-      { name: 'Ganhadores Quina', total: 14, amount: 15 },
-      { name: 'Ganhadores Quadra', total: 16, amount: 17 },
-      { name: 'Ganhadores Terno', total: 18, amount: 19 },
+      { name: 'Sena', total: 9, amount: 11 },
+      { name: 'Quina', total: 14, amount: 15 },
+      { name: 'Quadra', total: 16, amount: 17 },
+      { name: 'Terno', total: 18, amount: 19 },
     ],
   });
 
