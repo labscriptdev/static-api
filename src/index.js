d43752f8f79890e17350be21e058c0a02a796387
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-01-03 03:53:01 -0300

    megasena, lotofacil, quina, lotomania

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const { dataSave } = require('./utils');
 (async () => {
   await require('./loteria/megasena')();
   await require('./loteria/lotofacil')();
+  await require('./loteria/quina')();
+  await require('./loteria/lotomania')();
 
   dataSave('index.json', {
     last_update: dayjs().format(),
