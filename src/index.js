7de32d9a3dae8faf1a9af1013e88c901b7a9dff3
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-10-25 18:27:41 -0300

    Remove files, fix lotto schema

--- src/index.js
+++ src/index.js
@@ -4,17 +4,20 @@ const glob = require('fast-glob');
 
 (async () => {
   const openapi = openapiInit();
+  console.clear();
   
   let only = [
-    // path.join(__dirname, 'fipe', 'index.js'),
+    path.join(__dirname, 'loteria', 'index.js'),
   ];
 
   for(const file of await glob(`${__dirname}/**/index.js`)) {
     if (file == path.join(__dirname, 'index.js')) continue;
     if (only.length>0 && !only.includes(file)) continue;
     console.log(`Running ${file}`);
+    const a = require(file);
     await require(file)({ openapi });
   }
 
-  dataSave('openapi.json', openapi);
+  // console.log(openapi);
+  // dataSave('openapi.json', openapi);
 })();
--- src/loteria/index.js
+++ src/loteria/index.js
@@ -1,150 +1,294 @@
+const axios = require('axios');
+const https = require('https');
+const XLSX = require('xlsx');
+// const cheerio = require('cheerio');
+
 const { dataSave } = require('../utils');
-const { loteriaSchema } = require('./utils');
+
+const getLottoResults = async (params = {}) => {
+  params = {
+    id: 'mega-sena',
+    name: 'Mega-Sena',
+    rangeStart: 1,
+    rangeFinal: 60,
+    rangePerRow: 10,
+    parseRow: (rows) => rows,
+    ...params
+  };
+
+  const resp = await axios(`https://servicebus2.caixa.gov.br/portaldeloterias/api/resultados/download?modalidade=${params.name}`, {
+    responseType: 'arraybuffer',
+    httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+  });
+
+  const workbook = XLSX.read(resp.data);
+  const workSheet = workbook.Sheets[workbook.SheetNames[0]];
+
+  let data = [];
+  for(let i in workSheet) {
+    const [ col, row ] = i.split(/([A-Z]*)([0-9]*)/g).filter(v => v);
+    if (typeof data[row] == 'undefined') data[row] = {};
+    data[row][col] = workSheet[i]['v'];
+  }
+
+  data = data.filter(v => v);
+  data.splice(0, 1);
+  return data;
+};
 
 module.exports = async ({ openapi }) => {
   let loterias = [
     {
-      schema: {
-        id: 'mega-sena',
-        name: 'Mega-Sena',
-        number: 0,
-        date: 1,
-        numbersStart: 2,
-        numbersFinal: 7,
-        winners: [
-          { name: 'Sena', total: 8, amount: 11 },
-          { name: 'Quina', total: 9, amount: 12 },
-          { name: 'Quadra', total: 10, amount: 13 },
-        ],
-        rangeStart: 1,
-        rangeFinal: 60,
-        rangePerRow: 10,
+      id: 'super-sete',
+      name: 'Super Sete',
+      rangeStart: 1,
+      rangeFinal: 60,
+      rangePerRow: 10,
+      parseRow: (row) => {
+        return {
+          number: row['A'],
+          date: row['B'].split('/').reverse().join('-'),
+          numbers: [
+            row['C'],
+            row['D'],
+            row['E'],
+            row['F'],
+            row['G'],
+            row['H'],
+            row['I'],
+          ],
+        };
       },
     },
     {
-      schema: {
-        id: 'lotofacil',
-        name: 'Lotofacil',
-        number: 0,
-        date: 1,
-        numbersStart: 2,
-        numbersFinal: 16,
-        winners: [
-          { name: '15 números', total: 18, amount: 24 },
-          { name: '14 números', total: 21, amount: 25 },
-          { name: '13 números', total: 21, amount: 26 },
-          { name: '12 números', total: 22, amount: 27 },
-          { name: '11 números', total: 23, amount: 28 },
-        ],
-        rangeStart: 1,
-        rangeFinal: 25,
-        rangePerRow: 5,
+      id: 'dia-de-sorte',
+      name: 'Dia de Sorte',
+      rangeStart: 1,
+      rangeFinal: 60,
+      rangePerRow: 10,
+      parseRow: (row) => {
+        return {
+          number: row['A'],
+          date: row['B'].split('/').reverse().join('-'),
+          numbers: [
+            row['C'],
+            row['D'],
+            row['E'],
+            row['F'],
+            row['G'],
+            row['H'],
+            row['I'],
+          ],
+        };
       },
     },
     {
-      schema: {
-        id: 'quina',
-        name: 'Quina',
-        number: 0,
-        date: 1,
-        numbersStart: 2,
-        numbersFinal: 6,
-        winners: [
-          { name: 'Quina', total: 8, amount: 10 },
-          { name: 'Quadra', total: 11, amount: 12 },
-          { name: 'Terno', total: 13, amount: 14 },
-        ],
-        rangeStart: 1,
-        rangeFinal: 80,
-        rangePerRow: 10,
+      id: 'dupla-sena',
+      name: 'Dupla Sena',
+      rangeStart: 1,
+      rangeFinal: 60,
+      rangePerRow: 10,
+      parseRow: (row) => {
+        return {
+          number: row['A'],
+          date: row['B'].split('/').reverse().join('-'),
+          numbers: [
+            row['C'],
+            row['D'],
+            row['E'],
+            row['F'],
+            row['G'],
+            row['H'],
+            row['I'],
+          ],
+        };
       },
     },
     {
-      schema: {
-        id: 'lotomania',
-        name: 'Lotomania',
-        number: 0,
-        date: 1,
-        numbersStart: 2,
-        numbersFinal: 21,
-        winners: [
-          { name: '20 números', total: 23, amount: 31 },
-          { name: '19 números', total: 26, amount: 32 },
-          { name: '18 números', total: 27, amount: 33 },
-          { name: '17 números', total: 28, amount: 34 },
-          { name: '16 números', total: 29, amount: 35 },
-          { name: 'nenhum número', total: 30, amount: 36 },
-        ],
-        rangeStart: 1,
-        rangeFinal: 100,
-        rangePerRow: 10,
+      id: 'lotomania',
+      name: 'Lotomania',
+      rangeStart: 1,
+      rangeFinal: 100,
+      rangePerRow: 10,
+      parseRow: (row) => {
+        return {
+          number: row['A'],
+          date: row['B'].split('/').reverse().join('-'),
+          numbers: [
+            row['C'],
+            row['D'],
+            row['E'],
+            row['F'],
+            row['G'],
+            row['H'],
+            row['I'],
+            row['J'],
+            row['K'],
+            row['L'],
+            row['M'],
+            row['N'],
+            row['O'],
+            row['P'],
+            row['Q'],
+            row['R'],
+            row['S'],
+            row['T'],
+            row['U'],
+            row['V'],
+          ],
+        };
       },
     },
     {
-      schema: {
-        id: 'dupla-sena',
-        name: 'Dupla-Sena',
-        number: 0,
-        date: 1,
-        numbersStart: 2,
-        numbersFinal: 7,
-        winners: [
-          { name: 'Sena', total: 9, amount: 11 },
-          { name: 'Quina', total: 14, amount: 15 },
-          { name: 'Quadra', total: 16, amount: 17 },
-          { name: 'Terno', total: 18, amount: 19 },
-        ],
-        rangeStart: 1,
-        rangeFinal: 50,
-        rangePerRow: 10,
+      id: 'lotofacil',
+      name: 'Lotofacil',
+      rangeStart: 1,
+      rangeFinal: 25,
+      rangePerRow: 5,
+      parseRow: (row) => {
+        return {
+          number: row['A'],
+          date: row['B'].split('/').reverse().join('-'),
+          numbers: [
+            row['C'],
+            row['D'],
+            row['E'],
+            row['F'],
+            row['G'],
+            row['H'],
+            row['I'],
+            row['J'],
+            row['K'],
+            row['L'],
+            row['M'],
+            row['N'],
+            row['O'],
+            row['P'],
+            row['Q'],
+          ],
+        };
+      },
+    },
+    {
+      id: 'milionaria',
+      name: 'Milionária',
+      rangeStart: 1,
+      rangeFinal: 25,
+      rangePerRow: 5,
+      parseRow: (row) => {
+        return {
+          number: row['A'],
+          date: row['B'].split('/').reverse().join('-'),
+          numbers: [
+            row['C'],
+            row['D'],
+            row['E'],
+            row['F'],
+            row['G'],
+            row['H'],
+            row['I'],
+            row['J'],
+          ],
+        };
+      },
+    },
+    {
+      id: 'quina',
+      name: 'Quina',
+      rangeStart: 1,
+      rangeFinal: 80,
+      rangePerRow: 10,
+      parseRow: (row) => {
+        return {
+          number: row['A'],
+          date: row['B'].split('/').reverse().join('-'),
+          numbers: [
+            row['C'],
+            row['D'],
+            row['E'],
+            row['F'],
+            row['G'],
+            row['H'],
+          ],
+        };
+      },
+    },
+    {
+      id: 'mega-sena',
+      name: 'Mega-Sena',
+      rangeStart: 1,
+      rangeFinal: 60,
+      rangePerRow: 10,
+      parseRow: (row) => {
+        return {
+          number: row['A'],
+          date: row['B'].split('/').reverse().join('-'),
+          numbers: [
+            row['C'],
+            row['D'],
+            row['E'],
+            row['F'],
+            row['G'],
+            row['H'],
+          ],
+        };
       },
     },
   ];
 
-  dataSave('loteria/index.json', loterias.map(loteria => {
-    return {
-      id: loteria.schema.id,
-      name: loteria.schema.name,
-      rangeStart: loteria.schema.rangeStart,
-      rangeFinal: loteria.schema.rangeFinal,
-      rangePerRow: loteria.schema.rangePerRow,
-    };
-  }));
+  dataSave('loteria/index.json', loterias);
 
-  openapi.tagAdd('loteria');
-
-  openapi.pathAdd({
-    operationId: `loteria-index`,
-    path: '/loteria/index.json',
-    method: 'get',
-    tags: ['loteria'],
+  loterias.map(async loteria => {
+    const results = await getLottoResults(loteria);
+    let save = JSON.parse(JSON.stringify(loteria));
+    save.data = results.map(loteria.parseRow);
+    dataSave(`loteria/${loteria.id}.json`, save);
   });
+
+  // dataSave('loteria/index.json', loterias.map(loteria => {
+  //   return {
+  //     id: loteria.schema.id,
+  //     name: loteria.schema.name,
+  //     rangeStart: loteria.schema.rangeStart,
+  //     rangeFinal: loteria.schema.rangeFinal,
+  //     rangePerRow: loteria.schema.rangePerRow,
+  //   };
+  // }));
+
+  // openapi.tagAdd('loteria');
+
+  // openapi.pathAdd({
+  //   operationId: `loteria-index`,
+  //   path: '/loteria/index.json',
+  //   method: 'get',
+  //   tags: ['loteria'],
+  // });
   
-  loterias.forEach(loteria => {
-    loteriaSchema(loteria.schema);
+  // loterias.forEach(loteria => {
+  //   loteriaSchema(loteria.schema);
 
-    openapi.pathAdd({
-      operationId: `loteria-${loteria.schema.id}-index`,
-      path: `/loteria/${loteria.schema.id}/index.json`,
-      method: 'get',
-      tags: ['loteria'],
-    });
+  //   openapi.pathAdd({
+  //     operationId: `loteria-${loteria.schema.id}-index`,
+  //     path: `/loteria/${loteria.schema.id}/index.json`,
+  //     method: 'get',
+  //     tags: ['loteria'],
+  //   });
     
-    openapi.pathAdd({
-      operationId: `loteria-${loteria.schema.id}-number`,
-      path: `/loteria/${loteria.schema.id}/{number}.json`,
-      method: 'get',
-      tags: ['loteria'],
-      parameters: [
-        { name: 'number', in: 'path', required: true, schema: { type: 'string' }},
-      ],
-    });
+  //   openapi.pathAdd({
+  //     operationId: `loteria-${loteria.schema.id}-number`,
+  //     path: `/loteria/${loteria.schema.id}/{number}.json`,
+  //     method: 'get',
+  //     tags: ['loteria'],
+  //     parameters: [
+  //       { name: 'number', in: 'path', required: true, schema: { type: 'string' }},
+  //     ],
+  //   });
     
-    openapi.pathAdd({
-      operationId: `loteria-${loteria.schema.id}-all`,
-      path: `/loteria/${loteria.schema.id}/all.json`,
-      method: 'get',
-      tags: ['loteria'],
-    });
-  });
+  //   openapi.pathAdd({
+  //     operationId: `loteria-${loteria.schema.id}-all`,
+  //     path: `/loteria/${loteria.schema.id}/all.json`,
+  //     method: 'get',
+  //     tags: ['loteria'],
+  //   });
+  // });
 };
